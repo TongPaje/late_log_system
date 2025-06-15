567f98a5826d0d3f340ad1336ee84976946db68a
TongPaje <ernestopaje@gmail.com>
2025-06-15 20:41:33 +0800

    Update wsgi.py

--- late_log_system/wsgi.py
+++ late_log_system/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for late_log_system project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'late_log_system.settings')
 
 application = get_wsgi_application()
+
+# Serve static files only when in production and DEBUG is False
+if not settings.DEBUG:
+    application = StaticFilesHandler(application)
